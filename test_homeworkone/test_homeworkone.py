833bfc71668072c033360c3d5172d6d80a36c01f
huyq119 <yuqi.hu@gmail.com>
2021-02-24 15:32:31 +0800

    作业

---
+++ test_homeworkone/test_homeworkone.py
@@ -0,0 +1,59 @@
+import requests
+
+
+class TestHomeworkOne():
+
+    corpid = "wwe46394ef41c1fbe3"
+    corpsecret = "9iECP3c-crX1YAw1Is-WrkN2D8tfVOsZTGBch0p_BJQ"
+
+    def setup(self):
+
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={self.corpid}&corpsecret={self.corpsecret}"
+        r = requests.get(url)
+        self.access_token = r.json()["access_token"]
+
+    def test_demo_add(self):
+
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/user/create?access_token={self.access_token}"
+        body = {
+            "userid": "liudehua",
+            "name": "刘德华",
+            "alias": "AndyLau",
+            "mobile": "+86 13366136613",
+            "department": [1, 3],
+            "order": [3, 3],
+            "position": "首席运营官",
+            "gender": "1",
+            "email": "andylau@gzdev.com",
+        }
+        r = requests.post(url, json=body)
+        print(r.json())
+
+    def test_demo_update(self):
+
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token={self.access_token}"
+        body = {
+            "userid": "liudehua",
+            "name": "刘德华",
+            "department": [1,4],
+            "order": [1,1],
+            "position": "CTO",
+            "mobile": "13699765656",
+            "gender": "1",
+        }
+        r = requests.post(url, json=body)
+        print(r.json())
+
+    def test_demo_get(self):
+
+        userid = "liudehua"
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token={self.access_token}&userid={userid}"
+        r = requests.get(url)
+        print(r.json())
+
+    def test_demo_delete(self):
+
+        userid = "liudehua"
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/user/delete?access_token={self.access_token}&userid={userid}"
+        r = requests.get(url)
+        print(r.json())
